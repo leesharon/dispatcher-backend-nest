380a77ade7644113a2248b8c0970ff76a6f64845
Lee Sharon <lees@moveo.co.il>
2023-03-06 22:45:06 +0200

    refactor crud names

--- src/api/headlines/headlines.controller.ts
+++ src/api/headlines/headlines.controller.ts
@@ -4,29 +4,29 @@ import { CreateHeadlineDto } from './dtos/create-headline.dto'
 @Controller('/api/headlines')
 export class HeadlinesController {
     @Get()
-    getHeadlines(): string {
+    getAll(): string {
         return 'getHeadlines'
     }
 
     @Get(':id')
-    getHeadlineById(@Param('id') id: string): string {
+    getById(@Param('id') id: string): string {
         console.log('HeadlinesController ~ getHeadlineById ~ id:', id)
         return 'getHeadlineById'
     }
 
     @Post()
-    createHeadline(@Body() body: CreateHeadlineDto): string {
+    create(@Body() body: CreateHeadlineDto): string {
         console.log('HeadlinesController ~ createHeadline ~ body:', body)
         return 'createHeadline'
     }
 
     @Put(':id')
-    updateHeadline(): string {
+    update(): string {
         return 'updateHeadline'
     }
 
     @Delete(':id')
-    deleteHeadline(): string {
+    delete(): string {
         return 'deleteHeadline'
     }
 }
