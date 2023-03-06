29b6a36355a1998d47c5a3951874bbed86ed1ea7
Lee Sharon <lees@moveo.co.il>
2023-03-06 22:59:24 +0200

    headline service & repo creation

--- src/api/headlines/headlines.module.ts
+++ src/api/headlines/headlines.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { HeadlinesController } from './headlines.controller'
+import { HeadlinesService } from './headlines.service';
 
 @Module({
-    controllers: [HeadlinesController]
+    controllers: [HeadlinesController],
+    providers: [HeadlinesService]
 })
 export class HeadlinesModule { }
---
+++ src/api/headlines/headlines.repository.ts
@@ -0,0 +1,22 @@
+
+export class HeadlinesRepository {
+    async getAll() {
+        return 'getHeadlines'
+    }
+
+    async getById() {
+        return 'getHeadlineById'
+    }
+
+    async create() {
+        return 'createHeadline'
+    }
+
+    async update() {
+        return 'updateHeadline'
+    }
+
+    async delete() {
+        return 'deleteHeadline'
+    }
+}
